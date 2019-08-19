bb3354776ad8c585534f7922ab9c5f1b837494be
brad-bartlett <bradbartlett222@gmail.com>
2019-08-19 14:45:48 -0400

    associations

--- db/migrate/20190819173536_create_rentals.rb
+++ db/migrate/20190819173536_create_rentals.rb
@@ -1,6 +1,8 @@
 class CreateRentals < ActiveRecord::Migration[5.1]
   def change
     create_table :rentals do |t|
+      t.integer :item_id
+      t.string :item_type
 
       t.timestamps
     end
