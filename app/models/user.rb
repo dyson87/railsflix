b3fb9e00f308d1b40b6fd624d1769759827c5c8d
brad-bartlett <bradbartlett222@gmail.com>
2019-08-20 10:36:29 -0400

    validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     has_many :rentals
     has_many :movies, through: :rentals, source: :item, source_type: :Movie
     # has_many :reviews
+
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+
+
 end
