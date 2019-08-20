779f58a54395b801d9e96574af8a641dd4c078a0
brad-bartlett <bradbartlett222@gmail.com>
2019-08-20 15:48:41 -0400

    img_url

--- db/migrate/20190819173615_create_movies.rb
+++ db/migrate/20190819173615_create_movies.rb
@@ -4,6 +4,7 @@ class CreateMovies < ActiveRecord::Migration[5.1]
       t.string :title
       t.string :genre
       t.integer :year_released
+      t.string :img_url
       t.timestamps
     end
   end
