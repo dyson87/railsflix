cc1c2d4e733fbd5082bf7af011de7905403d087d
brad-bartlett <bradbartlett222@gmail.com>
2019-08-22 01:00:41 -0400

    rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -5,18 +5,19 @@ class RentalsController < ApplicationController
     end
 
     def show
-        @rental = Rental.find(rental_params)
+        @rental = Rental.find(item_type: params[:item_type], item_id: params[:item_id], user_id: params[:user_id])
         
     end
 
     def create
-        @rental = Rental.new(item_id: params[:item_id], user_id: params[:user_id], item_type: params[:item_type])
+        
+        @rental = Rental.new(item_type: params[:item_type], item_id: params[:item_id], user_id: params[:user_id])
         @rental.save
-            redirect_to rentals_path(@rental)
+        redirect_to rentals_path
     end
 
-    def rental_params
-        params.require(:rental).permit(:user_id, :item_id, :item_type)
-    end
+    # def rental_params
+    #     params.require(:rental).permit(:user_id, :item_id, :item_type)
+    # end
 
 end
